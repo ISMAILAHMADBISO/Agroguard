d7976a71a37dfb7ed104bff93c5ebc7718869de1
Ismail <askabuamaturrahman@gmail.com>
2026-06-09 16:44:43 +0100

    Remove duplicate root API route

--- api/[[...path]].ts
+++
@@ -1,8 +0,0 @@
-import app from "../../api-server/src/app";
-export const config = {
-  maxDuration: 60,
-};
-
-export default function handler(req: any, res: any) {
-  return (app as any)(req, res);
-}
